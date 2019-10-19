8f4aed58d6d03f787e2c27bb4eb64800cf504c81
RobertoBenjami <benjami@freemail.hu>
2019-10-19 13:04:19 +0200

    Update hx8347g.h

--- Drivers/lcd/hx8347g/hx8347g.h
+++ Drivers/lcd/hx8347g/hx8347g.h
@@ -1,10 +1,10 @@
-/* Orientácio:
-   - 0: 240x320 Reset gomb felül (portrait)
-   - 1: 320x240 Reset gomb bal oldalt (landscape)
-   - 2: 240x320 Reset gomb alul (portrait)
-   - 3: 320x240 Reset gomb jobb oldalt (landscape)
+/* Orientation:
+   - 0: 240x320 reset button in the top (portrait)
+   - 1: 320x240 reset button in the left (landscape)
+   - 2: 240x320 reset button in the bottom (portrait)
+   - 3: 320x240 reset button in the right (landscape)
 */
-#define  HX8347G_ORIENTATION       3
+#define  HX8347G_ORIENTATION       0
 
 /* Color mode
    - 0: RGB565 (R:bit15..11, G:bit10..5, B:bit4..0)
@@ -13,22 +13,25 @@
 #define  HX8347G_COLORMODE         0
 
 /* Analog touchscreen
-   - 0: Touchscreen tiltva
-   - 1: Touchscreen engedélyezve
+   - 0: touchscreen disabled
+   - 1: touchscreen enabled
 */
 #define  HX8347G_TOUCH             0
 
-/* Többszálas vagy megszakításból történö használat miatt az Lcd és a Touchscreen
-   egyidejü használata összeakadást tud okozni (mivel közös I/O eröforrásokat használ)
-   Ezzel a mutex-el ki lehet zárni az összeakadást.
-   Az Lcd függvényei kivárják a Touchscreen fefejeztét,
-   a touchscreen lekérdezés meg nem hajtodik végre, ha az Lcd éppen foglalt.
-   Figyelem: Ha az Lcd prioritása magasabb mint a Touchscreen-é, végtelen ciklusba kerülhet!
-   - 0: nincs védelem az LCD és a touchscreen egyidejüségének kizárására
-   - 1: van védelem az LCD és a touchscreen egyidejüségének kizárására
+/* Touchscreen calibration data for 4 orientations */
+#define  TS_CINDEX_0        {-2756000, 210932, -423352, 96135608, 269619, 1366, -1021658814};
+#define  TS_CINDEX_1        {-2756000, 269619, 1366, -1021658814, -210932, 423352, -754819608};
+#define  TS_CINDEX_2        {-2756000, -210932, 423352, -754819608, -269619, -1366, 142494814};
+#define  TS_CINDEX_3        {-2756000, -269619, -1366, 142494814, 210932, -423352, 96135608};
+
+/* For multi-threaded or intermittent use, Lcd and Touchscreen simultaneous use can cause confusion (since it uses common I/O resources)
+   Lcd functions wait for the touchscreen header, the touchscreen query is not executed when Lcd is busy.
+   Note: If the priority of the Lcd is higher than that of the Touchscreen, it may end up in an infinite loop!
+   - 0: multi-threaded protection disabled
+   - 1: multi-threaded protection enabled
 */
 #define  HX8347G_MULTITASK_MUTEX   0
 
-// HX8347G Size (fizikai felbontás, az alapértelmezett orientáciora vonatkoztatva)
+/* Physical resolution in default orientation */
 #define  HX8347G_LCD_PIXEL_WIDTH   240
 #define  HX8347G_LCD_PIXEL_HEIGHT  320
