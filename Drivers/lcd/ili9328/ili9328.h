bb9b77458e0f3a86f14f99b600699ee1a22473c3
RobertoBenjami <benjami@freemail.hu>
2019-10-19 13:18:43 +0200

    Update ili9328.h

--- Drivers/lcd/ili9328/ili9328.h
+++ Drivers/lcd/ili9328/ili9328.h
@@ -1,8 +1,8 @@
-/* Orientácio:
-   - 0: 240x320 Reset gomb felül (portrait)
-   - 1: 320x240 Reset gomb bal oldalt (landscape)
-   - 2: 240x320 Reset gomb alul (portrait)
-   - 3: 320x240 Reset gomb jobb oldalt (landscape)
+/* Orientation:
+   - 0: 240x320 reset button in the top (portrait)
+   - 1: 320x240 reset button in the left (landscape)
+   - 2: 240x320 reset button in the bottom (portrait)
+   - 3: 320x240 reset button in the right (landscape)
 */
 #define  ILI9328_ORIENTATION       0
 
@@ -13,22 +13,25 @@
 #define  ILI9328_COLORMODE         0
 
 /* Analog touchscreen
-   - 0: Touchscreen tiltva
-   - 1: Touchscreen engedélyezve
+   - 0: touchscreen disabled
+   - 1: touchscreen enabled
 */
 #define  ILI9328_TOUCH             0
 
-/* Többszálas vagy megszakításból történö használat miatt az Lcd és a Touchscreen
-   egyidejü használata összeakadást tud okozni (mivel közös I/O eröforrásokat használ)
-   Ezzel a mutex-el ki lehet zárni az összeakadást.
-   Az Lcd függvényei kivárják a Touchscreen fefejeztét,
-   a touchscreen lekérdezés meg nem hajtodik végre, ha az Lcd éppen foglalt.
-   Figyelem: Ha az Lcd prioritása magasabb mint a Touchscreen-é, végtelen ciklusba kerülhet!
-   - 0: nincs védelem az LCD és a touchscreen egyidejüségének kizárására
-   - 1: van védelem az LCD és a touchscreen egyidejüségének kizárására
+/* Touchscreen calibration data for 4 orientations */
+#define  TS_CINDEX_0        {-2776428, -209145, -6464, 123162313, 269742, -542860, 95435474}
+#define  TS_CINDEX_1        {-2776428, 269742, -542860, 95435474, 209145, 6464, -786728605}
+#define  TS_CINDEX_2        {-2776428, 209145, 6464, -786728605, -269742, 542860, -981116006}
+#define  TS_CINDEX_3        {-2776428, -269742, 542860, -981116006, -209145, -6464, 123162313}
+
+/* For multi-threaded or intermittent use, Lcd and Touchscreen simultaneous use can cause confusion (since it uses common I/O resources)
+   Lcd functions wait for the touchscreen header, the touchscreen query is not executed when Lcd is busy.
+   Note: If the priority of the Lcd is higher than that of the Touchscreen, it may end up in an infinite loop!
+   - 0: multi-threaded protection disabled
+   - 1: multi-threaded protection enabled
 */
 #define  ILI9328_MULTITASK_MUTEX   0
 
-// ILI9328 Size (fizikai felbontás, az alapértelmezett orientáciora vonatkoztatva)
+/* Physical resolution in default orientation */
 #define  ILI9328_LCD_PIXEL_WIDTH   240
 #define  ILI9328_LCD_PIXEL_HEIGHT  320
