6243f194cdcd062750cc9727535b09ede312f295
RobertoBenjami <benjami@freemail.hu>
2019-10-19 13:20:36 +0200

    Update ili9488.h

--- Drivers/lcd/ili9488/ili9488.h
+++ Drivers/lcd/ili9488/ili9488.h
@@ -19,10 +19,10 @@
 #define  ILI9488_TOUCH             0
 
 /* Touchscreen calibration data for 4 orientations */
-#define  TS_CINDEX_0        {3385020, 333702, -667424, 1243070964, -458484, -13002, 1806391572};
-#define  TS_CINDEX_1        {3385020, -458484, -13002, 1806391572, -333702, 667424, -163249584};
-#define  TS_CINDEX_2        {3385020, -333702, 667424, -163249584, 458484, 13002, -184966992};
-#define  TS_CINDEX_3        {3385020, 458484, 13002, -184966992, 333702, -667424, 1243070964};
+#define  TS_CINDEX_0        {3385020, 333702, -667424, 1243070964, -458484, -13002, 1806391572}
+#define  TS_CINDEX_1        {3385020, -458484, -13002, 1806391572, -333702, 667424, -163249584}
+#define  TS_CINDEX_2        {3385020, -333702, 667424, -163249584, 458484, 13002, -184966992}
+#define  TS_CINDEX_3        {3385020, 458484, 13002, -184966992, 333702, -667424, 1243070964}
 
 /* For multi-threaded or intermittent use, Lcd and Touchscreen simultaneous use can cause confusion (since it uses common I/O resources)
    Lcd functions wait for the touchscreen header, the touchscreen query is not executed when Lcd is busy.
