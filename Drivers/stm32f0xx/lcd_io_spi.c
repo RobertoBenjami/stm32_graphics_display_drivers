34d41aa20e41a55fa6487384e6260c42abaf6367
RobertoBenjami <robertodebenjami@gmail.com>
2021-01-09 01:06:08 +0100

    Update

--- Drivers/stm32f0xx/lcd_io_spi.c
+++ Drivers/stm32f0xx/lcd_io_spi.c
@@ -1,7 +1,7 @@
 /*
  * SPI LCD driver STM32F0
  * author: Roberto Benjami
- * version:  2021.01.06
+ * version:  2021.01.09
  *
  * - hardware, software SPI
  * - 3 modes (only TX, half duplex, full duplex)
@@ -160,6 +160,12 @@ void  LCD_IO_Delay(uint32_t c);
 #define DMAX_IFCR_CGIF(a)               DMAX_IFCR_CGIF_(a)
 
 //-----------------------------------------------------------------------------
+/* only transmit mode -> clear the DMA RX settings */
+#if     LCD_SPI_MODE == 0
+#undef  LCD_DMA_RX
+#define LCD_DMA_RX        0, 0, 0, 0
+#endif
+
 /* LCD_DMA_TX interrupt number */
 #if     (DMANUM(LCD_DMA_TX) == 1) && (DMACHN(LCD_DMA_TX) == 1)
 #define LCD_DMA_TX_IRQ         9
